6ecde2a461dd24ef13e8fa48a5f2359ffd22cddc
IvayloAnaniev <Zorc213@gmail.com>
2024-05-09 11:31:34 +0300

    asd

--- map.js
+++ map.js
@@ -3,13 +3,15 @@ require([
   "esri/WebMap",
   "esri/views/MapView",
   "esri/widgets/Home",
-  "esri/widgets/LayerList"],
+  "esri/widgets/LayerList",
+  "esri/widgets/BasemapGallery"],
   function (
     esriConfig,
     WebMap,
     MapView,
     Home,
-    LayerList
+    LayerList,
+    BasemapGallery,
   ) {
     esriConfig.apiKey = "AAPKc0d231b3177f4c7aa4f8fd4cc33d408eFr2xfZB6UcOjXi_RSt5Wf0akFMAjpgs38v03BbLMmZ6YAhkj3X7qwjsqhe6tv9sN";
     const webmap = new WebMap({
@@ -34,11 +36,33 @@ require([
     });
     view.ui.add("layer-list-btn", "top-right");
     view.ui.add(layerlist, "top-right");
+    view.ui.add("basemap-gallery-btn", "top-right")
 
-    document.getElementById("layer-list-btn").addEventListener("click", function() {
+    const basemapGalery = new BasemapGallery({
+      view
+    });
+
+    view.ui.add(basemapGalery, "top-right");
+
+    document.getElementById("layer-list-btn").addEventListener("click", function () {
+      toggleButton();
+    });
+
+    document.getElementById("basemap-gallery-btn").addEventListener("click", function () {
+      toggleButton();
+    });
+
+    function toggleButton() {
       const layerListEl = document.getElementsByClassName("esri-layer-list")[0];
       const currentProp = layerListEl.style.getPropertyValue("display");
-    
-      layerListEl.style.setProperty("display", currentProp == "none"? "block" : "none");
-    })
-  })
+
+      layerListEl.style.setProperty("display", currentProp == "none" ? "block" : "none");
+
+      const galleryEl = document.getElementsByClassName("esri-basemap-gallery")[0];
+      const currentPropGallery = galleryEl.style.getPropertyValue("display");
+      galleryEl.style.setProperty("display", currentPropGallery == "none" ? "block" : "none");
+    }
+
+
+
+  });
